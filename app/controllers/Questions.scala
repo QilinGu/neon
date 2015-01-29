21fd58e6755620b549fd6d5bebe22c528f99704d
Adam Sheehan <a.t.sheehan@gmail.com>
2015-01-28 22:45:43 -0500

    Add form to ask new questions.

--- app/controllers/Questions.scala
+++ app/controllers/Questions.scala
@@ -1,7 +1,12 @@
 package controllers
 
-import play.api.mvc.{Action, Controller, RequestHeader}
-import models.{Question, User}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc.{ Action, Controller, RequestHeader }
+import models.{ Question, User }
+import play.mvc.Http.Response
+
+case class QuestionParams(title: String, body: String)
 
 object Questions extends Controller {
   def index = Action {
@@ -10,13 +15,30 @@ object Questions extends Controller {
   }
 
   def show(id: Long) = Action {
-    implicit request => {
-      Question.find(id) match {
-        case Some(question) => Ok(views.html.questions.show(question))
-        case None => NotFound
+    implicit request =>
+      {
+        Question.find(id) match {
+          case Some(question) => Ok(views.html.questions.show(question))
+          case None => NotFound
+        }
       }
-    }
   }
+
+  def newForm = Action { implicit request =>
+    Ok(views.html.questions.newForm(questionForm))
+  }
+
+  def create = Action { implicit request =>
+    questionForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.questions.newForm(formWithErrors)),
+      params => saveAndRedirect(params))
+  }
+
+  def saveAndRedirect(params: QuestionParams) =
+    Question.create(params) match {
+      case Some(question) => Redirect(routes.Questions.show(question.id))
+      case None => InternalServerError("Oops")
+    }
 
   implicit def user(implicit request: RequestHeader): Option[User] = {
     request.session.get("user.id") match {
@@ -24,4 +46,11 @@ object Questions extends Controller {
       case None => None
     }
   }
+
+  val questionMapping = mapping(
+    "title" -> nonEmptyText,
+    "body" -> text
+  )(QuestionParams.apply)(QuestionParams.unapply)
+
+  val questionForm = Form(questionMapping)
 }
--- app/models/Question.scala
+++ app/models/Question.scala
@@ -1,8 +1,9 @@
 package models
 
-import anorm.{SQL, SqlQuery}
+import anorm.{ SQL, SqlQuery }
 import play.api.Play.current
 import play.api.db.DB
+import controllers.QuestionParams
 
 case class Question(id: Long, title: String, body: String)
 
@@ -10,26 +11,46 @@ object Question {
   val allQuery = SQL("SELECT * FROM questions");
 
   def all = DB.withConnection {
-    implicit connection => {
-      allQuery().map {
-        row =>
-          Question(row[Long]("id"),
-                   row[String]("title"),
-                   row[String]("body"))
-      }.toList
-    }
+    implicit connection =>
+      {
+        allQuery().map {
+          row =>
+            Question(
+              row[Long]("id"),
+              row[String]("title"),
+              row[String]("body")
+            )
+        }.toList
+      }
   }
 
   val findQuery = SQL("SELECT * FROM questions WHERE id = {id}")
 
   def find(id: Long) = DB.withConnection {
-    implicit connection => {
-      findQuery.on("id" -> id).map {
-        row =>
-          Question(row[Long]("id"),
-                   row[String]("title"),
-                   row[String]("body"))
-      }.singleOpt
+    implicit connection =>
+      {
+        findQuery.on("id" -> id).map {
+          row =>
+            Question(
+              row[Long]("id"),
+              row[String]("title"),
+              row[String]("body")
+            )
+        }.singleOpt
+      }
+  }
+
+  val createQuestionStatement =
+    SQL("INSERT INTO questions (title, body, created_at) VALUES ({title}, {body}, NOW()) RETURNING id")
+
+  def create(params: QuestionParams): Option[Question] = {
+    DB.withConnection { implicit connection =>
+      createQuestionStatement.on(
+        "title" -> params.title,
+        "body" -> params.body
+      ).map {
+          row => Question(row[Long]("id"), params.title, params.body)
+        }.singleOpt()
     }
   }
 }
