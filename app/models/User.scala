3921fe716341bc343a0d06b7128f145c0bfbc706
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-10 19:29:50 -0500

    Refactor User model.

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,59 +1,41 @@
 package models
 
-import anorm.{ SQL, SqlQuery }
+import anorm.{ SQL, SqlQuery, Row }
 import play.api.Play.current
 import play.api.db.DB
 
 case class User(id: Long, githubId: Long, username: String)
 
 object User {
-  val findQuery = SQL("SELECT * FROM users WHERE id = {id}")
-
-  def find(id: Long) = DB.withConnection { implicit connection =>
-    {
-      findQuery.on("id" -> id).map {
-        row =>
-          User(
-            row[Long]("id"),
-            row[Long]("github_id"),
-            row[String]("username")
-          )
-      }.singleOpt
-    }
-  }
-
-  val findByGithubIdQuery = SQL("SELECT * FROM users WHERE github_id = {github_id}")
-
-  def findByGithubId(githubId: Long) = DB.withConnection { implicit connection =>
-    {
-      findByGithubIdQuery.on("github_id" -> githubId).map {
-        row =>
-          User(
-            row[Long]("id"),
-            row[Long]("github_id"),
-            row[String]("username")
-          )
-      }.singleOpt()
-    }
-  }
-
-  val createUserStatement =
-    SQL("INSERT INTO users (github_id, username, created_at) VALUES ({github_id}, {username}, NOW()) RETURNING id")
-
-  def create(githubId: Long, username: String): Option[User] = {
+  def find(id: Long) =
     DB.withConnection { implicit connection =>
-      createUserStatement.on("github_id" -> githubId, "username" -> username).map {
-        row => User(row[Long]("id"), githubId, username)
-      }.singleOpt()
-                     }
-  }
-
-  def findOrCreateByGithubProfile(githubId: Long, username: String): Option[User] = {
-    DB.withConnection { implicit connection =>
-      findByGithubId(githubId) match {
-        case user: Some[User] => user
-        case None => create(githubId, username)
-      }
+      SQL("SELECT * FROM users WHERE id = {id}")
+        .on("id" -> id).map(mapUser _).singleOpt
     }
-  }
+
+  def findByGithubId(githubId: Long) =
+    DB.withConnection { implicit connection =>
+      SQL("SELECT * FROM users WHERE github_id = {github_id}")
+        .on("github_id" -> githubId).map(mapUser _).singleOpt
+    }
+
+  def findOrCreateByGithubProfile(githubId: Long, username: String): Option[User] =
+    DB.withConnection { implicit connection =>
+      findByGithubId(githubId).orElse(create(githubId, username))
+    }
+
+  def create(githubId: Long, username: String): Option[User] =
+    DB.withConnection { implicit connection =>
+      insertStatement(githubId, username)
+        .map(row => User(row[Long]("id"), githubId, username))
+        .singleOpt
+    }
+
+  private def insertStatement(githubId: Long, username: String) =
+    SQL("INSERT INTO users (github_id, username, created_at) VALUES " +
+      "({github_id}, {username}, NOW()) RETURNING id")
+      .on("github_id" -> githubId, "username" -> username)
+
+  private def mapUser(row: Row): User =
+    User(row[Long]("id"), row[Long]("github_id"), row[String]("username"))
 }
