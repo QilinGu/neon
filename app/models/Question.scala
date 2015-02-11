0323152dc1d6679fae90b8779ace201b57d7b92c
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-10 19:38:50 -0500

    Refactor Question model.

--- app/models/Question.scala
+++ app/models/Question.scala
@@ -1,36 +1,35 @@
 package models
 
-import anorm.{SQL, SqlQuery}
+import anorm.{SQL, SqlQuery, Row}
 import play.api.Play.current
 import play.api.db.DB
 
 case class Question(id: Long, title: String, body: String)
 
 object Question {
-  def all = DB.withConnection { implicit connection =>
-    findAllQuery().map { row => mapQuestion(row) }.toList
-  }
+  def all =
+    DB.withConnection { implicit connection =>
+      SQL("SELECT * FROM questions")().map(mapQuestion _).toList
+    }
 
-  def find(id: Long) = DB.withConnection { implicit connection =>
-    findOneQuery.on("id" -> id).map { row => mapQuestion(row) }.singleOpt
-  }
+  def find(id: Long) =
+    DB.withConnection { implicit connection =>
+      SQL("SELECT * FROM questions WHERE id = {id}")
+        .on("id" -> id).map(mapQuestion _).singleOpt
+    }
 
-  val findAllQuery = SQL("SELECT * FROM questions");
-  val findOneQuery = SQL("SELECT * FROM questions WHERE id = {id}")
-  val createQuestionStatement =
+  def create(title: String, body: String, userId: Long): Option[Question] =
+    DB.withConnection { implicit connection =>
+      insertStatement(title, body, userId)
+        .map(row => Question(row[Long]("id"), title, body))
+        .singleOpt
+    }
+
+  def insertStatement(title: String, body: String, userId: Long) =
     SQL("INSERT INTO questions (title, body, user_id, created_at) " +
       "VALUES ({title}, {body}, {user_id}, NOW()) RETURNING id")
+      .on("title" -> title, "body" -> body, "user_id" -> userId)
 
-  def create(title: String, body: String, userId: Long): Option[Question] = {
-    DB.withConnection { implicit connection =>
-      createQuestionStatement.on(
-        "title" -> title,
-        "body" -> body,
-        "user_id" -> userId
-      ).map { row => Question(row[Long]("id"), title, body) }.singleOpt()
-    }
-  }
-
-  private def mapQuestion(row: anorm.Row): Question =
+  private def mapQuestion(row: Row): Question =
     Question(row[Long]("id"), row[String]("title"), row[String]("body"))
 }
